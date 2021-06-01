c5ba81ce3b18a6db15723ab647a9edc14f275fe2
Cleo Beka <cleobeka@hotmail.com>
2021-06-01 16:46:44 +0300

    Updates

---
+++ precache-manifest.bedcb08075c5c64e9d9122e09b8d84cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97bded6692c3b5caf548ded350374e12",
+    "url": "/auction-app/index.html"
+  },
+  {
+    "revision": "a04bb94e9c13433de381",
+    "url": "/auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4ab664c94e1ea4785949",
+    "url": "/auction-app/static/js/2.00eae5ac.chunk.js"
+  },
+  {
+    "revision": "a04bb94e9c13433de381",
+    "url": "/auction-app/static/js/main.a8601922.chunk.js"
+  },
+  {
+    "revision": "f3bf5b93f7e7136135cd",
+    "url": "/auction-app/static/js/runtime-main.ad8c3a94.js"
+  }
+]);
